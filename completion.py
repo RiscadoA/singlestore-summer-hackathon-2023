c7d00f6ae30e94df5fc51f4233602a589ae46b22
Filipe Ligeiro Silva <flsilva@singlestore.com>
2023-07-18 09:26:40 +0100

    chore: moved function to appropriate place

--- completion.py
+++ completion.py
@@ -1,19 +1,9 @@
 import openai
 import os
-import tiktoken
 
 OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-3.5-turbo")
-ENCODING_NAME = os.getenv("ENCODING_NAME", "cl100k_base")
 
 class CompletionTools:
-    _encoding = ENCODING_NAME
-
-    def num_tokens_from_string(self, string):
-        """Returns the number of tokens in a text string"""
-        encoding = tiktoken.get_encoding(ENCODING_NAME)
-        num_tokens = len(encoding.encode(string))
-        return num_tokens
-
     def prompt(self, prompt):
         result = openai.ChatCompletion.create(
             model=OPENAI_MODEL,
--- embedding.py
+++ embedding.py
@@ -1,8 +1,10 @@
 import openai
 import os
+import tiktoken
 
 class EmbeddingTools():
-    model = os.getenv("EMBEDDING_MODEL", "text-embedding-ada-002")
+    _encoding = os.getenv("ENCODING_NAME", "cl100k_base")
+    _model = os.getenv("EMBEDDING_MODEL", "text-embedding-ada-002")
 
     def __init__(self, conn):
         super().__init__()
@@ -14,11 +16,17 @@ class EmbeddingTools():
 
     def get_embedding(self, text):
         """Returns the vector for semantic search, using the OpenAI embedding API"""
-        return openai.Embedding.create(input=[text], model=self.model)["data"][0]["embedding"]
+        return openai.Embedding.create(input=[text], model=self._model)["data"][0]["embedding"]
 
     def get_embeddings(self, vector):
         """get_embedding but mapped to a vector of inputs"""
         return list(map(self.get_embedding, vector))
+
+    def num_tokens_from_string(self, string):
+        """Returns the number of tokens in a text string"""
+        encoding = tiktoken.get_encoding(self._encoding)
+        num_tokens = len(encoding.encode(string))
+        return num_tokens
 
     def semantic_search(self, query, limit=3):
         """Using the SingleStore connector, see phrases that are related to the one we are searching for"""
